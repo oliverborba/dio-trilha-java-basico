b82d607513da71b868f8c268dac2ffd46b3963ed
Lucas <lucas_borba@outlook.com>
2023-02-19 21:01:51 -0300

    Metodos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        int numero = 5;
+
+        numero++;
+
+        System.out.println(numero);
+        System.out.println(++numero);
+
+    }
+}
